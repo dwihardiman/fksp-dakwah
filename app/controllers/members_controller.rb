59379d7f1728a0cf2814f00b25608a7c67512f15
Dwi Hardiman <dwihardyman@gmail.com>
2016-11-18 04:25:16 +0700

    Membuat halaman dasbor untuk member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,28 @@
 class MembersController < ApplicationController
+  before_action :find_member, only: [:show, :dasbor]
+  before_action :find_episode, only: [:show, :dasbor]
+
   def index
     @members = Member.all.order("created_at DESC")
   end
 
   def show
-    @member = Member.find(params[:id])
+  end
+
+  def dasbor
+  end
+
+  private
+
+  def find_episode
     @episodes = Episode.where(member_id: @member).order("created_at DESC")
+  end
+
+  def find_member
+    if params[:id].nil?
+      @member = current_member
+    else
+      @member = Member.find(params[:id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
 
   resources :members, only: [:index, :show] do
     resources :episodes
   end
 
+  authenticated :member do
+    root 'members#dasbor', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
