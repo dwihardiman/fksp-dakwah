aaf3a08a1fe5ba3dbfe4d13a98a564cfaacefba8
Dwi Hardiman <dwihardyman@gmail.com>
2016-11-17 15:16:24 +0700

    Membuat episode model & asosiasi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117015404) do
+ActiveRecord::Schema.define(version: 20161117080331) do
+
+  create_table "episodes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "member_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["member_id"], name: "index_episodes_on_member_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
