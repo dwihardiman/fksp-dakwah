62f3cd9f67a57ba15150c2c5632581c8082a8664
Dwi Hardiman <dwihardyman@gmail.com>
2016-11-21 22:19:18 +0700

    Mengubah list member pada halaman utama menjadi dinamis

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @members = Member.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
     respond_to do |format|
       format.html
       format.json { @episodes = Episode.search(params[:term]) }
