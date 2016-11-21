24e01288479ef54bbbec99578331532c08fb8670
Dwi Hardiman <dwihardyman@gmail.com>
2016-11-22 05:17:19 +0700

    Menambahkan .count untuk hitung jumlah member & jumlah episode yang sudah terkumpul

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @members = Member.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+    @episodes = Episode.all.order("created_at DESC")
     respond_to do |format|
       format.html
       format.json { @episodes = Episode.search(params[:term]) }
