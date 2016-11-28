a1e7aaead21bed2ab3b10fdf112e140159a2e702
Dwi Hardiman <dwihardyman@gmail.com>
2016-11-29 06:49:43 +0700

    Menambahkan fungsi dan halaman hasil pencarian

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @members = Member.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
-    @episodes = Episode.all.order("created_at DESC")
+
+    if params[:search]
+      @episodes = Episode.search(params[:search]).order("created_at DESC")
+    else
+      @episodes = Episode.all.order("created_at DESC")
+    end
+
     respond_to do |format|
       format.html
       format.json { @episodes = Episode.search(params[:term]) }
